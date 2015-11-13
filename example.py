af0570581531a270df097f57d4d7275670815295
tjof2 <tjof2@cam.ac.uk>
2015-11-13 09:59:00 +0000

    Updated Python script

--- example.py
+++ example.py
@@ -8,11 +8,11 @@ mat = scipy.io.loadmat('../Dataset.mat')
 D = np.array(mat['var2'], order='F')
 D = D.astype(np.double, copy = False)
 
-A1, E1 = pyrosl.rosl(D, method='subsample', sampling=500, rank=5, reg=0.02, tol=1E-5, iters=50, verbose=False)
+A1, E1 = pyrosl.rosl(D,'subsample', 500, 5, 0.02, 1E-5, 50, True)
 
 print np.linalg.norm(D - A1 - E1, 'fro') / np.linalg.norm(D, 'fro')
 
-A2, E2 = pyrosl.rosl(D, method='full', sampling=-1, rank=5, reg=0.015, tol=1E-5, iters=50, verbose=False)
+A2, E2 = pyrosl.rosl(D, 'full', rank=5, reg=0.015)
 
 print np.linalg.norm(D - A2 - E2, 'fro') / np.linalg.norm(D, 'fro')
 
