05e97cdc35a6432c2d3418496391952cd24dde3f
bm424 <bm424@cam.ac.uk>
2015-11-16 14:18:56 +0000

    Update example.py to reflect the changes made to pyrosl.py

--- example.py
+++ example.py
@@ -11,21 +11,34 @@ D = np.array(mat['var2'], order='F')
 D = D.astype(np.double, copy = False)
 
 # Run the sub-sampled version
-print 'Starting ROSL+'
-A1, E1 = pyrosl.rosl(D,'subsample', (72*72,500) , 3, 0.02, 1E-5, 50, True)
-print 'Finished ROSL+'
+ss_rosl = pyrosl.ROSL( 
+    method='subsample',
+    sampling = (72*72,500),
+    rank = 3,
+    reg = 0.02,
+    tol = 1E-5,
+    iters = 50,
+    verbose = True
+)
+
+ss_rosl.fit_transform(D)
 print '---'
 
 # Run the full ROSL algorithm
-#print 'Starting ROSL'
-#A2, E2 = pyrosl.rosl(D, 'full', rank=3, reg=0.015)
-#print 'Finished ROSL'
-#print '---'
+full_rosl = pyrosl.ROSL(
+    method = 'full',
+    rank = 3,
+    reg = 0.015,
+   )
+print 'Starting ROSL'
+full_rosl.fit_transform(D)
+print 'Finished ROSL'
+print '---'
 
 # Output some numbers
-error1 = np.linalg.norm(D - A1 - E1, 'fro') / np.linalg.norm(D, 'fro')
-error2 = np.linalg.norm(D - A2 - E2, 'fro') / np.linalg.norm(D, 'fro')
-error3 = np.linalg.norm(A1 - A2, 'fro') / np.linalg.norm(A2, 'fro')
+error1 = np.linalg.norm(D - ss_rosl.model_ - ss_rosl.residuals_, 'fro') / np.linalg.norm(D, 'fro')
+error2 = np.linalg.norm(D - full_rosl.model_ - full_rosl.residuals_, 'fro') / np.linalg.norm(D, 'fro')
+error3 = np.linalg.norm(ss_rosl.model_ - full_rosl.model_, 'fro') / np.linalg.norm(full_rosl.model_, 'fro')
 print 'Subsampled ROSL+ error: %.3f' % error1
 print 'Full ROSL error:        %.3f' % error2
 print 'ROSL/ROSL+ comparison:  %.3f' % error3
