28dad228c0b1fc3cbd86fec25bfa642f16f78211
bm424 <bm424@cam.ac.uk>
2015-11-16 16:58:06 +0000

    Add a few utility functions to the example for testing

--- example.py
+++ example.py
@@ -2,18 +2,41 @@ import os, sys, warnings
 import scipy.io
 import numpy as np
 import pyrosl
+from hyperspy import signals
+import hyperspy.hspy as hs
 
 print '---'
 
+GAAS_35_SOURCE =\
+                {
+                'directory': '../../../source_data/gaas_images/precessed35mrad',
+                'filetype' : 'tif',
+                'shape' : (100, 30, 144, 144),
+                }
+                
+def load_original_data(directory='.', filetype='bmp', shape=(0,0,0,0), norm=False):
+    nm = hs.load('{}/*.{}'.format(directory, filetype), stack=True)
+    nm.change_dtype(float)
+    nm = signals.Image(nm.data.reshape(shape))
+    return nm
+
+def matrixify(image_data, n_components=None):
+    d = image_data    
+    if len(d.shape)==4:
+        return d.reshape(d.shape[0]*d.shape[1],d.shape[2]*d.shape[3])
+    elif len(d.shape)==3:
+        return d.reshape(d.shape[0],d.shape[1]*d.shape[2])
+    else:
+        raise NotImplementedError("That doesn't look like a standard image.")
+        
 # Load the data
-mat = scipy.io.loadmat('../Dataset.mat')
-D = np.array(mat['var2'], order='F')
+D = matrixify(load_original_data(**GAAS_35_SOURCE).data)
 D = D.astype(np.double, copy = False)
 
 # Run the sub-sampled version
 ss_rosl = pyrosl.ROSL( 
     method='subsample',
-    sampling = (72*72,500),
+    sampling = (36*36,500),
     rank = 3,
     reg = 0.02,
     tol = 1E-5,
@@ -21,7 +44,7 @@ ss_rosl = pyrosl.ROSL(
     verbose = True
 )
 
-ss_rosl.fit_transform(D)
+#ss_rosl.fit_transform(D)
 print '---'
 
 # Run the full ROSL algorithm
