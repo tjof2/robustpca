a419b2441a9969985f805356810af0a6fa890eeb
Tom Furnival <tjof2@cam.ac.uk>
2019-11-10 17:25:54 +0000

    Initial commit of Python wrapper

--- pyrosl/__init__.py
+++ pyrosl/__init__.py
@@ -15,3 +15,16 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with RobustPCA.  If not, see <http://www.gnu.org/licenses/>.
+
+import logging
+
+_logger = logging.getLogger(__name__)
+
+__author__ = "Tom Furnival"
+__copyright__ = "Copyright 2015-2019, Tom Furnival"
+__credits__ = ["Tom Furnival"]
+__license__ = "GPLv3"
+__version__ = "0.2.0"
+__maintainer__ = "Tom Furnival"
+__email__ = ""
+__status__ = "Development"
