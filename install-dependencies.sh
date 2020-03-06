462ab8fceb58156e3a7e004b955818d393e517bb
Tom Furnival <6626247+tjof2@users.noreply.github.com>
2020-03-06 13:50:45 +0000

    Update install-dependencies.sh

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,23 +1,6 @@
 #!/bin/sh
 #   This script builds from source:
 #       - Armadillo 9.800.2
-#
-# Copyright 2015-2020 Tom Furnival
-#
-# This file is part of PGURE-SVT.
-#
-# PGURE-SVT is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# PGURE-SVT is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with PGURE-SVT.  If not, see <http://www.gnu.org/licenses/>.
 
 set -ex
 
@@ -27,4 +10,3 @@ cd armadillo-9.800.2
 cmake .
 make
 sudo make install
-
